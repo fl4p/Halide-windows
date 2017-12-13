316bc2e959fcefc6495a9467f3493d65dedf9fed
Fabian Schlieper <fabian@schlieper.email>
2017-12-13 13:29:09 +0100

    added scripts

---
+++ halide-build/!build.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" /maxcpucount:8 /m /t:Build /p:Configuration=Release .\ALL_BUILD.vcxproj
+pause
--- halide-build/!configure.cmd
+++ halide-build/!configure.cmd
@@ -1,10 +1,3 @@
 REM cmake -DCMAKE_INSTALL_PREFIX=../llvm-install -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_TARGETS_TO_BUILD=X86;ARM;NVPTX;AArch64;Mips;Hexagon -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_BUILD_32_BITS=OFF -DCMAKE_BUILD_TYPE=Release ../llvm -G "Visual Studio 14 Win64" -DPYTHON_EXECUTABLE=C:\Users\Fabian\AppData\Local\Android\sdk\ndk-bundle\prebuilt\windows-x86_64\bin\python.exe
 
-REM cmake -Thost=x64 -DLLVM_DIR=../llvm-install/lib/cmake/llvm -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 14 Win64" ../Halide
-
-cmake -DLLVM_DIR="C:\Program Files\LLVM" -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 14 Win64" ../Halide
-
-
-pause
-
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /m /t:Build /p:Configuration=Release .\ALL_BUILD.vcxproj
+cmake -Thost=x64 -DLLVM_DIR=../llvm-install/lib/cmake/llvm -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 15 Win64" ../Halide
